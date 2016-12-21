0ac1df4e056211265fcd81a05af94073302d7bd3
Alan R. Rogers <rogers@anthro.utah.edu>
2016-12-20 23:05:17 -0700

    .

--- test/xdtnorm.c
+++ test/xdtnorm.c
@@ -16,29 +16,42 @@
 #include <gsl/gsl_rng.h>
 #include <gsl/gsl_randist.h>
 #include <time.h>
+#include <math.h>
+#include <assert.h>
 
 #include "dtnorm.h"
+
+void test(int nreps, double mu, double sigma, double a, double b, gsl_rng *rng);
+
+void test(int nreps, double mu, double sigma, double a, double b, gsl_rng *rng) {
+    int i;
+    double min = HUGE_VAL;
+    double max = -HUGE_VAL;
+    double x;
+    for(i=0; i < nreps; ++i) {
+        x = dtnorm(mu, sigma, a, b, rng);
+        min = fmin(min, x);
+        max = fmax(max, x);
+    }
+    assert(min >= a);
+    assert(max <= b);
+}
 
 int main(void) {
     double      a;              // Left bound
     double      b;              // Right bound
     double      mu = 2;         // Mean
     double      sigma = 3;      // Standard deviation
-    double      x;              // Output of rtnorm
     int         nreps = 1e6;    // Number of random variates to generate
 
-    //--- GSL random init ---
     gsl_rng    *rng = gsl_rng_alloc(gsl_rng_taus);
     gsl_rng_set(rng, (unsigned long) time(NULL));
 
-    //--- generate and display the random numbers ---
-    printf("underlying distribution: Normal(%lf, %lf)\n", mu, sigma);
-    for(int k = 0; k < nreps; k++) {
-        a = gsl_ran_flat(rng, mu - 5.0*sigma, mu + 5.0*sigma);
-        b = gsl_ran_flat(rng, a + 0.001*sigma, a + 5.0*sigma);
-        x = dtnorm(mu, sigma, a, b, rng);
-        printf("%lf\n", x);
-    }
+    a = 1.0;
+    b = 1.5;
+    test(nreps, mu, sigma, a*sigma, b*sigma, rng);
+
+    printf("dtnorm OK\n");
 
     gsl_rng_free(rng);
 
