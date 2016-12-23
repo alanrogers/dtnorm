f3a1001c4e990714116cd873d3216c07a1039e90
Alan R. Rogers <rogers@anthro.utah.edu>
2016-12-23 15:57:23 -0700

    Update documentation

--- src/dtnorm.c
+++ src/dtnorm.c
@@ -1,33 +1,43 @@
-//  Pseudorandom numbers from a truncated Gaussian distribution.
-//
-// This code was translated by Alan R. Rogers into C, based on the C++
-// original written by G. Dolle and V Mazet, which is available at
-// http://miv.u-strasbg.fr/mazet/rtnorm/rtnormCpp.zip
-//
-//  This implements an extension of Chopin's algorithm detailed in
-//  N. Chopin, "Fast simulation of truncated Gaussian distributions",
-//  Stat Comput (2011) 21:275-288
-//
-//  Copyright (C) 2012 Guillaume Dollé, Vincent Mazet
-//  (LSIIT, CNRS/Université de Strasbourg)
-//  Version 2012-07-04, Contact: vincent.mazet@unistra.fr
-//
-//  06/07/2012:
-//  - first launch of rtnorm.cpp
-//
-//  Licence: GNU General Public License Version 2
-//  This program is free software; you can redistribute it and/or modify it
-//  under the terms of the GNU General Public License as published by the
-//  Free Software Foundation; either version 2 of the License, or (at your
-//  option) any later version. This program is distributed in the hope that
-//  it will be useful, but WITHOUT ANY WARRANTY; without even the implied
-//  warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//  GNU General Public License for more details. You should have received a
-//  copy of the GNU General Public License along with this program; if not,
-//  see http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
-//
-//  Depends: LibGSL
-//  OS: Unix based system
+/**
+ * @file dtnorm.c
+ * @author Alan R. Rogers
+ * @brief Pseudorandom numbers from a truncated Gaussian distribution.
+ *
+ * This code began with C++ original written by G. Dolle and V Mazet,
+ * which is available at
+ * http://miv.u-strasbg.fr/mazet/rtnorm/rtnormCpp.zip.  That original
+ * code implemented several methods, including Chopin's algorithm
+ * detailed in N. Chopin, "Fast simulation of truncated Gaussian
+ * distributions", Stat Comput (2011) 21:275-288. Different methods
+ * were used in different regions of parameter space.
+ *
+ * My own numerical experiments indicated that this method can be
+ * improved by changing the criteria used in selecting algorithms. In
+ * some regions of parameter space, it is best to use Robert's method
+ * (Robert, CP. 1995. Statistics and Computing, 5(2):121). The current
+ * code implements the revised method in C.
+ *
+ *  Copyright (C) 2012 Guillaume Dollé, Vincent Mazet
+ *  (LSIIT, CNRS/Université de Strasbourg)
+ *  Version 2012-07-04, Contact: vincent.mazet@unistra.fr
+ *
+ *  06/07/2012:
+ *  - first launch of rtnorm.cpp
+ *
+ *  Licence: GNU General Public License Version 2
+ *  This program is free software; you can redistribute it and/or modify it
+ *  under the terms of the GNU General Public License as published by the
+ *  Free Software Foundation; either version 2 of the License, or (at your
+ *  option) any later version. This program is distributed in the hope that
+ *  it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ *  warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details. You should have received a
+ *  copy of the GNU General Public License along with this program; if not,
+ *  see http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+ *
+ *  Depends: LibGSL
+ *  OS: Unix based system
+ */
 
 #include <math.h>
 #include <stdio.h>
